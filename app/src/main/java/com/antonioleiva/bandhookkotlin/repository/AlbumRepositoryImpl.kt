d2b1750835f7b8b98067745890c1d672bdf96e9f
Raul Raja <raulraja@gmail.com>
2017-03-01 00:05:36 +0100

    Refactored getAlbum by Id call until Interactor. Next: remove event bus

--- app/src/main/java/com/antonioleiva/bandhookkotlin/repository/AlbumRepositoryImpl.kt
+++ app/src/main/java/com/antonioleiva/bandhookkotlin/repository/AlbumRepositoryImpl.kt
@@ -16,17 +16,42 @@
 
 package com.antonioleiva.bandhookkotlin.repository
 
+import com.antonioleiva.bandhookkotlin.*
+import com.antonioleiva.bandhookkotlin.domain.entity.Album
+import com.antonioleiva.bandhookkotlin.domain.entity.BizException.*
 import com.antonioleiva.bandhookkotlin.domain.repository.AlbumRepository
 import com.antonioleiva.bandhookkotlin.repository.dataset.AlbumDataSet
+import org.funktionale.collections.tail
 
 class AlbumRepositoryImpl(val albumDataSets: List<AlbumDataSet>) : AlbumRepository {
 
-    override fun getAlbum(id: String) = albumDataSets
-            .map { it.requestAlbum(id) }
-            .firstOrNull { it != null }
+    /** Trampolined (async) recursion until all DataSets are exhausted or a right result is found */
+    fun <E, A> recurseDataSets(currentDataSets: List<AlbumDataSet>, acc: Result<E, A>, f : (AlbumDataSet) -> Result<E, A>): Result<E, A> =
+        if (currentDataSets.isEmpty()) { acc }
+        else {
+            val current = currentDataSets.get(0)
+            f(current).recoverWith {
+                recurseDataSets(currentDataSets.tail(), f(current), f) }
+            }
 
-    override fun getTopAlbums(artistId: String?, artistName: String?) = albumDataSets
-                .map { it.requestTopAlbums(artistId, artistName) }
-                .firstOrNull { it.isNotEmpty() }
-                ?: emptyList()
+
+    override fun getAlbum(id: String): Result<AlbumNotFound, Album> =
+        recurseDataSets(
+                currentDataSets = albumDataSets,
+                acc = ResultT.raiseError<AlbumNotFound, Album>(AlbumNotFound(id)),
+                f = { it.requestAlbum(id) }
+        )
+
+
+    override fun getTopAlbums(artistId: String?, artistName: String?): List<Album> {
+        for (dataSet in albumDataSets) {
+            val result = dataSet.requestTopAlbums(artistId, artistName)
+            if (result.isNotEmpty()) {
+                return result
+            }
+        }
+
+        return emptyList()
+    }
+
 }
